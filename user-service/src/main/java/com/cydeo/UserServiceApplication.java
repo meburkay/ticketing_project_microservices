d94b5a8db13c1a6114a8f1b29c41c6f24bf4d602
Mehmet Emre BURKAY <86316791+meburkay@users.noreply.github.com>
2023-12-25 12:14:15 +0300

    user-service registeration to the eureka server part is completed

--- user-service/src/main/java/com/cydeo/UserServiceApplication.java
+++ user-service/src/main/java/com/cydeo/UserServiceApplication.java
@@ -3,10 +3,13 @@ package com.cydeo;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+//We add this for defining that this service will be eureka discovery client.Other than the server to the other services we must add this annotation. For the server we added the @EnableDiscoveryServer annotation.
+@EnableDiscoveryClient
 public class UserServiceApplication {
 
     public static void main(String[] args) {
