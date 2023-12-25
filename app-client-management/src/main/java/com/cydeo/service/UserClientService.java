3bf2e23eb33bb16091137e7c6e3545c0bf440952
Mehmet Emre BURKAY <86316791+meburkay@users.noreply.github.com>
2023-12-25 12:50:39 +0300

    task-service and project-service registeration to the eureka server configuration part is completed

--- app-client-management/src/main/java/com/cydeo/service/UserClientService.java
+++ app-client-management/src/main/java/com/cydeo/service/UserClientService.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 //We create this service/project for the consuming operations. All the consuming operations will be done from one place which is this service
-@FeignClient(value = "user-service",url = "http://localhost:9090")
+@FeignClient(value = "user-service")//We delte the url here because we register to the eureka server and afterwards it is not important for as the port/url. We give the user-service name to eureka server and it gives us a service.
 public interface UserClientService {
 
     @GetMapping("/api/v1/user/{username}")
